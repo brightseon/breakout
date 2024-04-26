e5e0004fea259cf5a92554e358642e6ef5311b10
seon-mac <public.seon@gmail.com>
2024-04-26 21:32:26 +0900

    [TASK-530] x, y 정의

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+
 function draw() {
     ctx.beginPath();
-    ctx.arc(50, 50, 10, 0, Math.PI * 2);
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
     ctx.fillStyle = '#0095DD';
     ctx.fill();
     ctx.closePath();
