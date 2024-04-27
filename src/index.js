9bd2173ec03caec957a7bf8575923955dcda490e
seon-mac <public.seon@gmail.com>
2024-04-27 15:42:36 +0900

    [TASK-531] ballRadius 변수화

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById('canvas');
 const startButton = document.getElementById('start');
 const ctx = canvas.getContext('2d');
 
+const ballRadius = 10;
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = 2;
@@ -9,7 +10,7 @@ let dy = -2;
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = '#0095DD';
     ctx.fill();
     ctx.closePath();
