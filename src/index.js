6c82f855a13b4ba67251628987796615102336e5
seon-mac <public.seon@gmail.com>
2024-04-29 09:41:56 +0900

    [TASK-532] 캔버스에 패들 표시

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ const startButton = document.getElementById('start');
 const ctx = canvas.getContext('2d');
 
 const ballRadius = 10;
+const paddleWidth = 75;
+const paddleHeight = 10;
+let paddleX = (canvas.width - paddleWidth) / 2;
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = '#0095DD';
+    ctx.fill();
+    ctx.closePath();
+}
 
 function drawBall() {
     ctx.beginPath();
@@ -18,6 +29,7 @@ function drawBall() {
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawPaddle();
     drawBall();
 
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
